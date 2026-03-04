5fdcdf36b63d9ac0a5cb0f7f7b387796f8efb1df
Iacopo Zavolta <iachizav@gmail.com>
2026-03-04 17:27:01 +0100

    Implement satellite image visualization in R
    Added R code for visualizing satellite images using various color palettes and plotting techniques.

--- Code_/02_visualization.R
+++ Code_/02_visualization.R
@@ -1,1 +1,41 @@
+#R code for visualizing satellite images
+library(terra) # package for using spatial data
+library(imageRy) # package devoted to satellite images
+library(viridis) 
 
+im.list()
+
+# Sentinel-2 bands
+# https://gisgeography.com/sentinel-2-bands-combinations/
+
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+#R è casesensitive attenzione alle maiuscole e minuscole 
+
+# Changing colors
+cl <- colorRampPalette(c("lightsalmon", "yellow", "mediumpurple1"))(100)
+plot(b2, col=cl)
+
+# Small number of nuances
+cl <- colorRampPalette(c("lightsalmon", "yellow", "mediumpurple1"))(3)
+plot(b2, col=cl)
+
+# Using viridis to change colors
+plot(b2, col=inferno(100))
+plot(b2, col=mako(100))
+
+# Exercise: assign a greycolor palete to the image
+cl <- colorRampPalette(c("dark gray", "gray", "light gray"))(100)   
+plot(b2, col=cl)
+
+# par
+par(mfrow=c(1,2))
+plot(b2, col=inferno(100))
+plot(b2, col=cl)
+
+dev.off()
+
+# im.multiframe
+im.multiframe(1,2)
+plot(b2, col=inferno(100))
+plot(b2, col=cl)
