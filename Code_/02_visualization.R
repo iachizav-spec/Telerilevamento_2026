9c818a9b95e00e6b6f88174e412a7a11dabf920b
Iacopo Zavolta <iachizav@gmail.com>
2026-03-05 15:09:03 +0100

    Update 02_visualization.R

--- Code_/02_visualization.R
+++ Code_/02_visualization.R
@@ -43,3 +43,46 @@ dev.off()
 im.multiframe(1,2)
 plot(b2, col=inferno(100))
 plot(b2, col=cl)
+
+# bands
+b3 = im.import("sentinel.dolomites.b3.tif")
+b4 = im.import("sentinel.dolomites.b4.tif")
+b8 = im.import("sentinel.dolomites.b8.tif")
+
+par(mfrow=c(1,4))
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+dev.off()
+
+im.multiframe(1,4)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+# Exercise: plot the bands using im.multiframe() one on top of the other
+im.multiframe(4,1)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+im.multiframe(2,2)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+
+im.multiframe(2,2)
+cl1 <- colorRampPalette(c("firebrick4", "red", "brown1"))(100)   
+cl2 <- colorRampPalette(c("dark green", "green", "light green"))(100)   
+cl3 <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)   
+cl4 <- colorRampPalette(c("darkorchid", "orchid", "lightorchid"))(100)   
+plot(b2, col=cl1)
+plot(b3, col=cl2)
+plot(b4, col=cl3)
+plot(b8, col=cl4)
