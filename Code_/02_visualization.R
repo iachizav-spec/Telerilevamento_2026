9bba870eeb826074373e8d293cc49d6cf072a6b8
Iacopo Zavolta <iachizav@gmail.com>
2026-03-05 15:17:51 +0100

    Refactor visualization and add new plots for bands
    Refactor visualization code to improve clarity and organization. Added new plots for imported bands and adjusted color palettes.

--- Code_/02_visualization.R
+++ Code_/02_visualization.R
@@ -70,11 +70,6 @@ plot(b3)
 plot(b4)
 plot(b8)
 
-im.multiframe(2,2)
-plot(b2)
-plot(b3)
-plot(b4)
-plot(b8)
 
 
 im.multiframe(2,2)
@@ -86,3 +81,55 @@ plot(b2, col=cl1)
 plot(b3, col=cl2)
 plot(b4, col=cl3)
 plot(b8, col=cl4)
+
+sent = c(b2, b3, b4, b8)
+plot(sent, col=cl)
+
+names(sent) = c("b2-blue", "b3-green", "b4-red", "b8-NIR")
+sent
+
+plot(sent, col=cl)
+plot(sent)
+
+names(sent) = c("b2blue", "b3green", "b4red", "b8NIR")
+dev.off()
+plot(sent$b8NIR)
+
+plot(sent[[4]])
+
+# importing several bands altogether
+sentdol = im.import("sentinel.dolomites")
+
+# How to import severla sets altogether
+pairs(sentdol)]
+plot(sentdol[[4]])
+plot(sentdol[[4]], col=inferno(100))
+
+# viridis
+plot(sentdol, col=viridis(100))
+plot(sentdol, col=mako(100))
+plot(sentdol, col=magma(100))
+
+# Viridis colors:
+# https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+
+# 1 - band 2 (blue)
+# 2 - band 3 (green)
+# 3 - band 4 (red)
+# 4 - band 8 (NIR)
+
+# Natural colors
+im.plotRGB(sentdol, r=3, g=2, b=1)
+
+# False colors
+im.plotRGB(sentdol, r=4, g=3, b=2)
+im.plotRGB(sentdol, r=3, g=4, b=2)
+im.plotRGB(sentdol, r=2, g=4, b=3)
+im.plotRGB(sentdol, r=3, g=4, b=1)
+
+im.multiframe(1,2)
+im.plotRGB(sentdol, r=2, g=4, b=3)
+im.plotRGB(sentdol, r=3, g=4, b=1)
+
+dev.off()
+im.plotRGB(sentdol, r=3, g=2, b=4)
